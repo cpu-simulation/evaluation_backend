acb9cdbd0458a8f19fd216a843267b94f8e2e63d
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-05-23 22:28:26 +0330

    IMP => team model

--- src/rest_service/team/models.py
+++ src/rest_service/team/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Team(models.Model):
+
+    id = models.IntegerField(
+        _("Id"),
+        primary_key=True,
+        db_column="team_id",
+        db_index=True,
+        auto_created=True
+    )
+    name = models.CharField(
+        _("Team name"),
+        max_length=50,
+        )
+    #TODO: Add more fields
+
+    class Meta:
+        verbose_name = _("Team")
+        verbose_name_plural = _("Teams")
+        db_table = "teams"
+        
+
+    def __str__(self):
+        return self.name
