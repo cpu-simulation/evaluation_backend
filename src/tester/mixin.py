1108c96e3d1a215e8f324ef3331ae51bc7a6daf0
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-04 20:43:20 +0330

    IMP => mixins

---
+++ src/tester/mixin.py
@@ -0,0 +1,27 @@
+import json
+from models import Team, Scenario, Result
+from config import Session
+import uuid
+
+class ConsumerMixin:
+    def callback(self, body):
+        message = json.loads(body)
+        id = message["team_id"]
+        id = uuid.UUID(id) 
+        team = Session.query(Team).filter_by(id=id).first()
+        scenarios = Session.query(Scenario).all()
+        for scenario in scenarios:
+            result = self.__test(team=team, scenario=scenario)
+            Session.add(result)
+        Session.commit()
+
+    def __test(self, team:Team, scenario:Scenario )-> Result:
+        print(f"TEST {team}, with {scenario}")
+        print("NOT IMPLEMENTED")
+        r = Result()
+        r.scenario_id = scenario.id
+        r.score = 100
+        r.team_id = team.id
+        r.average_time = 12
+        r.status = Result.StatusEnum.P
+        return r
