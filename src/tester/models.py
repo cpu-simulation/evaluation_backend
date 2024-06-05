d759c835d63a97eec050a45e29a85aa51732d6c3
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-05 21:13:33 +0330

    IMP => Scenario steps

--- src/tester/models.py
+++ src/tester/models.py
@@ -13,9 +13,32 @@ class Scenario(Base):
     name: Mapped[str] = MappedColumn()
     weight: Mapped[int] = MappedColumn()
     results: Mapped[List["Result"]] = relationship()
+    steps: Mapped[List["ScenarioStep"]] = relationship()
 
     def __repr__(self):
         return f"<Scenario(id={self.id}, name={self.name}, weight={self.weight})>"
+
+
+class ScenarioStep(Base):
+    class TypeEnum(enum.Enum):
+        M_R = "Memory Read"
+        M_W = "Memory Write"
+        R_R = "Register Read"
+        R_W = "Register Write"
+        C_C = "Cpu Compile"
+        C_E = "Cpu Execute"
+
+    __tablename__ = "scenario_steps"
+    id: Mapped[int] = MappedColumn(primary_key=True)
+    scenario_id: Mapped[int] = MappedColumn(ForeignKey("scenarios.id"))
+    scenario: Mapped["Scenario"] = relationship(back_populates="steps")
+    name: Mapped[str] = MappedColumn()
+    type: Mapped[Enum] = MappedColumn(Enum(TypeEnum))
+    input: Mapped[JSON] = MappedColumn(type_=JSON)
+    output: Mapped[JSON] = MappedColumn(type_=JSON)
+
+    def __repr__(self):
+        return f"<ScenarioStep:{self.name}>"
 
 
 class Team(Base):
@@ -43,10 +66,7 @@ class Result(Base):
     __tablename__ = "results"
 
     id: Mapped[uuid.UUID] = MappedColumn(
-        "result_id",
-        types.Uuid,
-        primary_key=True,
-        default=uuid.uuid4
+        "result_id", types.Uuid, primary_key=True, default=uuid.uuid4
     )
     team_id: Mapped[uuid.UUID] = MappedColumn(ForeignKey("teams.team_id"))
     team: Mapped["Team"] = relationship(back_populates="results")
@@ -55,7 +75,9 @@ class Result(Base):
     average_time: Mapped[int] = MappedColumn()
     score: Mapped[int] = MappedColumn()
     created_at = Column(DateTime(timezone=True), default=func.now())
-    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), default=func.now(), onupdate=func.now()
+    )
 
     def __repr__(self) -> str:
         return f"<Result team={self.team.name}, scenario={self.scenario_id}, score={self.score}>"
