fd04d7c169fc9e7170c2a2a3885222c502881a8c
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-11 08:38:43 +0330

    IMP => logger

--- src/tester/config/conf.py
+++ src/tester/config/conf.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session, declarative_base
 import sqlalchemy as sa
-import os
 from pathlib import Path
+import os, json, logging, atexit
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Database and orm configuration
 DB = sa.create_engine(os.environ.get("DB_URL"))
@@ -11,3 +13,15 @@ Base = declarative_base()
 # RabbitMQ configuration
 RABBIT = os.environ.get("RABBITMQ_URL", "rabbitmq")
 TEST_QUEUE = os.environ.get("TEST_QUEUE", "Test_Queue")
+
+logger_file = open("config/logger.json")
+LOGGING = json.loads(logger_file.read())
+logger_file.close()
+
+def setup_logging():
+    logging.config.dictConfig(LOGGING) 
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
+
