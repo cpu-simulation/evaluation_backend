d5a3ba37dbc4addeb9449f8f51ad88c1fef13d63
amin <109164319+AminMasoudi@users.noreply.github.com>
2024-06-15 10:09:46 +0330

    IMP => team urls + result hyperlink (#22)

--- src/rest_service/evaluate/serializers.py
+++ src/rest_service/evaluate/serializers.py
@@ -3,6 +3,8 @@ from .models import Result, Scenario
 
 class ResultsSerializer(serializers.ModelSerializer):
     team_name = serializers.CharField(source="team.name")
+    scenario = serializers.HyperlinkedRelatedField(view_name="scenario-detail", read_only=True)
+    status = serializers.SerializerMethodField()
     class Meta:
         model = Result
         fields = (
@@ -13,7 +15,9 @@ class ResultsSerializer(serializers.ModelSerializer):
             "average_time",
             "score"
         )
-
+    def get_status(self, obj):
+        return Result.RESULT_STATUS(obj.status).name
+    
 class ScenarioSerializer(serializers.ModelSerializer):
     number_of_steps = serializers.IntegerField()
     class Meta:
