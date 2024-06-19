29e82e0ea8365ad3543cb91ad55c9c86423548b7
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-19 13:45:11 +0330

    IMP =>  consumer removed from main

--- src/tester/main.py
+++ src/tester/main.py
@@ -1,12 +1,14 @@
 import logging.config
-import pika, os, sys
-from test import *
+import os, sys
 from config import Base, DB, RABBIT, TEST_QUEUE, setup_logging
-from mixin import ConsumerMixin
+from controller import Consumer
 import json
 import logging
+
+
 MOCK = json.dumps({"team_id": "7c90d362-c443-40da-b428-fab41656dd0f"})  #FIXME
 logger = logging.getLogger(__name__)
+
 
 def main():
     try:
@@ -21,37 +23,10 @@ def main():
             sys.exit(0)
         except:
             os._exit(0)
+
     except Exception as e:
         logger.error(e)
         raise e
-
-
-class Consumer(ConsumerMixin):
-    __connection_host: str
-    __queue: str
-    __inited = False
-
-    def __init__(self, host, queue) -> None:
-        self.__connection_host = host
-        self.__queue = queue
-
-    def connect(self):
-        self.__connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.__connection_host)
-        )
-        self.__channel = self.__connection.channel()
-        self.__channel.basic_consume(
-            queue=self.__queue, on_message_callback=self.callback
-        )
-        self.__inited = True
-
-    def callback(self, ch, method, properties, body):
-        return super().callback(body)
-
-    def start_consuming(self):
-        assert self.__inited, "Consumer not initialized"
-        self.__channel.start_consuming()
-
 
 if __name__ == "__main__":
     setup_logging()
