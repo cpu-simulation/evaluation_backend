2e336eefe70ea17f6078759ac515c7f50dffa8ac
Sepehr Moniri <68424394+hiddenSm@users.noreply.github.com>
2024-06-22 13:14:53 +0330

    Update src/rest_service/team/serializers.py
    Co-authored-by: amin <109164319+AminMasoudi@users.noreply.github.com>

--- src/rest_service/team/serializers.py
+++ src/rest_service/team/serializers.py
@@ -6,4 +6,4 @@ class TeamSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Team
-        fields = ["id", "name", "members", "total_score", "type", "template"]
+        fields = ["id", "name", "members", "total_score", "has_website", "used_template"]
