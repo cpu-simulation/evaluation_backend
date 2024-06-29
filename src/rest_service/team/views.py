0be9cdcceb5eb62c3482132951f88630d280cd2d
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-29 22:53:08 +0330

    IMP => fixxes

--- src/rest_service/team/views.py
+++ src/rest_service/team/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.db.models import Avg
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 # Create your views here.
 
 from .models import Team
-from .serializers import TeamSerializers
+from .serializers import TeamSerializers, TeamTestSerialzers
 
 # Create your views here.
 
@@ -15,3 +18,15 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
     ).all()
 
     serializer_class = TeamSerializers
+
+    @action(detail=True, methods=['post'])
+    def test(self, request, pk=None):
+        team = get_object_or_404(Team, pk=pk)
+        serializer = TeamTestSerialzers
+
+        if serializer.is_valid():
+            ...
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
