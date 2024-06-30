1008e38c926c38e9d4633b6f16ad39be52c22c35
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-30 17:55:47 +0330

    IMP => endpoint

--- src/rest_service/team/serializers.py
+++ src/rest_service/team/serializers.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+
 from rest_framework import serializers
+
 from .models import Team
+from .task_manager import push_task
 
 class TeamSerializers(serializers.ModelSerializer):
     total_score = serializers.FloatField()
@@ -8,9 +12,15 @@ class TeamSerializers(serializers.ModelSerializer):
         model = Team
         fields = ["id", "name", "members", "total_score", "has_website", "used_template"]
 
-class TeamTestSerialzers(serializers.ModelSerializer):
-    test_field = serializers.CharField(required=True)
-
+class TeamTestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['test_field']
+        fields = []
+
+    def save_task(self):
+        
+        msg = {
+            "team_id": f"{self.instance.id}",
+            "url": f"api.{self.instance.name}.dev"
+        }
+        push_task(msg=msg)
