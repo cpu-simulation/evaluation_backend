b35e3602f96bcecab9d7801093822f4d1f567d42
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-06-30 12:49:37 +0330

    IMP => endpoint

--- src/rest_service/team/views.py
+++ src/rest_service/team/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.db.models import Avg
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Team
-from .serializers import TeamSerializers, TeamTestSerialzers
+from .serializers import TeamSerializers, TeamTestSerializer
 
 # Create your views here.
 
@@ -19,14 +19,17 @@ class TeamViewSet(viewsets.ReadOnlyModelViewSet):
 
     serializer_class = TeamSerializers
 
+    def get_serializer_class(self):
+        if self.action == "test":
+            return TeamTestSerializer
+        return super().get_serializer_class()
+
+
     @action(detail=True, methods=['post'])
     def test(self, request, pk=None):
         team = get_object_or_404(Team, pk=pk)
-        serializer = TeamTestSerialzers
-
-        if serializer.is_valid():
-            ...
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save_task()
+        return Response(serializer.data, status=status.HTTP_200_OK)
