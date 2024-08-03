31aeec0ea8616a00e44af1897e9a622c4a1cf5e5
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-03 22:07:31 +0330

    RENAME => new structure

---
+++ src/refactored_tester/config/database.py
@@ -0,0 +1,27 @@
+import sqlalchemy as sa
+from sqlalchemy import URL
+from sqlalchemy.orm import Session, declarative_base
+
+import os
+
+Base = declarative_base()
+
+def get_db():
+    DB_conf = {
+        "drivername":"mysql+pymysql",
+        "database": os.environ.get("MYSQL_DB"),
+        "host":os.environ.get("DB_URL", "mysql"),
+        "password":os.environ.get("MYSQL_PASSWORD"),
+        "port":int(os.environ.get("MYSQL_PORT", "3306")),
+        "username":os.environ.get("MYSQL_USER"),
+        }
+    DB = db_connection(**DB_conf)
+    Base.metadata.create_all(DB)
+    session = Session(DB)
+    return session
+
+
+def db_connection(**kwargs):
+    return sa.create_engine(URL.create(
+        **kwargs
+    ))
