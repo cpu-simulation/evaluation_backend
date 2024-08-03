a158f7e8ee774031df8ab73f475bd1a3681b3139
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-03 22:27:30 +0330

    IMP => ...

--- src/refactored_tester/main.py
+++ src/refactored_tester/main.py
@@ -1,7 +1,10 @@
 import os
-from .database import get_db
+
+from core.controller import Controller
+from core.pod_handler import PodHandler
+from .config.database import get_db
 from .dependency import DependencyContainer
-from .queue_handler import PikaQueueHandler
+from .core.queue_handler import PikaQueueHandler
 
 
 def main(dp_container: DependencyContainer) -> None:
@@ -14,8 +17,14 @@ def main(dp_container: DependencyContainer) -> None:
 if __name__=="__main__":
     dp_container = DependencyContainer()
     dp_container.db = get_db()
+    dp_container.pod_handler = PodHandler()
+    dp_container.controller = Controller(
+        db=dp_container.db,
+        pod_handler=dp_container.pod_handler
+        )
     dp_container.queue_handler = PikaQueueHandler(
         host=os.environ.get("RABBITMQ_URL", "rabbitmq"),
-        queue=os.environ.get("TEST_QUEUE", "Test_Queue")
+        queue=os.environ.get("TEST_QUEUE", "Test_Queue"),
+        callback=dp_container.controller
     )
     main(dp_container)
