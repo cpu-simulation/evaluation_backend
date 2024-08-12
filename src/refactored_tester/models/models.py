e3be1b3763787561033f9fad830389b66be1e14d
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-12 09:11:19 +0330

    IMP => models: Scenario

--- src/refactored_tester/models/models.py
+++ src/refactored_tester/models/models.py
@@ -1,7 +1,24 @@
+from sqlalchemy.orm import (
+    Mapped,
+    DeclarativeBase,
+    MappedAsDataclass,
+)
+from sqlalchemy.schema import PrimaryKeyConstraint
 
-from sqlalchemy.orm import DeclarativeBase
 
-
-class Base(DeclarativeBase):
+class Base(
+    DeclarativeBase,
+    MappedAsDataclass,
+):
     pass
 
+
+class Scenario(Base):
+    __tablename__ = "scenarios"
+    __table_args__ = PrimaryKeyConstraint(name=id)
+    name: Mapped[str]
+    weight: Mapped[int]
+    id: Mapped[int]
+
+    def __repr__(self):
+        return f"<Scenario(id={self.id}, name={self.name}, weight={self.weight})>"
