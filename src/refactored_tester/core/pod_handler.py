a83d397de52f98000087e36170cbfabf078489f5
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-12 12:30:52 +0330

    IMP => PodHandler

--- src/refactored_tester/core/pod_handler.py
+++ src/refactored_tester/core/pod_handler.py
@@ -1,7 +1,26 @@
 from abc import ABC, abstractmethod
+import requests
+import time
+
 
 class AbstractPodHandler(ABC):
-    ...
+    @abstractmethod
+    def __init__(self, *args, **kwargs) -> None:
+        ...
+    @abstractmethod
+    def connect(self)->None:
+        ...
 
 class PodHandler(AbstractPodHandler):
-    ...
+    def __init__(self, host:str, *args, **kwargs) -> None:
+        self.host = host
+        self.connection_checked = False
+    
+    def connect(self):
+        try:
+            requests.head(f"http:/{self.host}:8000")
+        except:
+            time.sleep(5)
+            raise Exception("host not reachable")
+        else:
+            self.connection_checked = True
