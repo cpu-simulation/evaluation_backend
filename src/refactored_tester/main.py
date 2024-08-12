c18a709d5ecc558bcf1482a41b606ed664c88253
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-12 13:46:00 +0330

    IMP => ...

--- src/refactored_tester/main.py
+++ src/refactored_tester/main.py
@@ -6,25 +6,28 @@ from .config.database import get_db
 from .dependency import DependencyContainer
 from .core.queue_handler import PikaQueueHandler
 
-
 def main(dp_container: DependencyContainer) -> None:
     # logic:
     # setup controller with db-handler and pod-handler
     # connect it to queue handler
+    controller = Controller(
+        db=dp_container.db,
+        pod_handler=dp_container.pod_handler
+    )
+
+    queue_handler = PikaQueueHandler(
+        host=dp_container.RABBITMQ_URL,
+        queue=dp_container.TEST_QUEUE,
+        callback=controller
+    )
+    queue_handler.connect()
+    queue_handler.start_consuming()
     
-    ...
 
 if __name__=="__main__":
     dp_container = DependencyContainer()
     dp_container.db = get_db()
-    dp_container.pod_handler = PodHandler()
-    dp_container.controller = Controller(
-        db=dp_container.db,
-        pod_handler=dp_container.pod_handler
-        )
-    dp_container.queue_handler = PikaQueueHandler(
-        host=os.environ.get("RABBITMQ_URL", "rabbitmq"),
-        queue=os.environ.get("TEST_QUEUE", "Test_Queue"),
-        callback=dp_container.controller
-    )
+    dp_container.pod_handler = PodHandler
+    dp_container.TEST_QUEUE = os.environ.get("TEST_QUEUE", "Test_Queue")
+    dp_container.RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "rabbitmq")
     main(dp_container)
