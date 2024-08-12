4bbf6a183951c20126ed27304a33fc50e0ef618c
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-12 13:45:29 +0330

    UPD => DI_contaner

--- src/refactored_tester/dependency.py
+++ src/refactored_tester/dependency.py
@@ -5,6 +5,8 @@ from core.pod_handler import AbstractPodHandler
 from core.queue_handler import AbstractQueueHandler
 
 class DependencyContainer:
+    TEST_QUEUE: str
+    RABBITMQ_URL: str
     db: Session
     queue_handler: AbstractQueueHandler
     pod_handler: AbstractPodHandler
