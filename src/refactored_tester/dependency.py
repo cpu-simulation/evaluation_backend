9f51b467abee145b3e83f512e4fc31eda5f35aa5
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-14 14:03:18 +0330

    UPD => di_container to use pydantic and sessionmaker

--- src/refactored_tester/dependency.py
+++ src/refactored_tester/dependency.py
@@ -1,13 +1,15 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
 
 from core.controller import Controller
 from core.pod_handler import AbstractPodHandler
 from core.queue_handler import AbstractQueueHandler
 
-class DependencyContainer:
+class DependencyContainer(BaseModel):
+    model_config = {"arbitrary_types_allowed":True}
+
     TEST_QUEUE: str
     RABBITMQ_URL: str
-    db: Session
+    db: sessionmaker
     queue_handler: AbstractQueueHandler
     pod_handler: AbstractPodHandler
-    controller: Controller
