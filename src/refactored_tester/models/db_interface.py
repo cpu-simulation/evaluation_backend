e1dd46e39554ee797f93ff403deec774c2937b75
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-14 13:57:33 +0330

    IMP => find_scenario_steps for db_interface

--- src/refactored_tester/models/db_interface.py
+++ src/refactored_tester/models/db_interface.py
@@ -20,3 +20,10 @@ class DataBaseInterface:
             session.expunge(scenarios)
         return scenarios
 
+    @staticmethod
+    def find_scenario_steps(Session:sessionmaker ,scenario_id):
+        with Session.begin() as session:
+            query = sa.select(ScenarioStep).where(ScenarioStep.scenario_id==scenario_id).order_by(ScenarioStep.step)
+            scenario_steps = list(session.scalars(query).all())
+            session.expunge(scenario_steps)
+        return scenario_steps
