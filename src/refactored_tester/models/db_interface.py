52951c4ab12bf234948dc51b82b65b8ecd079a60
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-14 13:57:16 +0330

    IMP => db interface

--- src/refactored_tester/models/db_interface.py
+++ src/refactored_tester/models/db_interface.py
@@ -1,18 +1,22 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 import sqlalchemy as sa
-from .models import Scenario, Team
+from .models import Scenario, Team, ScenarioStep
 
 
 class DataBaseInterface:
     @staticmethod
-    def find_team_by_id(session:Session, team_id) -> Team|None:
-        with session.begin():
+    def find_team_by_id(Session:sessionmaker, team_id) -> Team|None:
+        with Session.begin() as session:
             query = sa.select(Team).where(Team.id==team_id)
             team = session.scalars(query).first()
             session.expunge(team)
         return team
 
     @staticmethod
-    def find_scenarios(session: Session) -> list:
-        ...   
-    
+    def find_scenarios(Session: sessionmaker) -> list[Scenario]:
+        with Session.begin() as session:
+            query = sa.select(Scenario)
+            scenarios = list(session.scalars(query).all())
+            session.expunge(scenarios)
+        return scenarios
+
